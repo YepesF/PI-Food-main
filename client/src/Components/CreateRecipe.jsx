50b5ecd602c3e39839fca7051f26ea645b8aab97
YepesF <yeplief@gmail.com>
2022-07-21 06:07:22 -0500

    UpdateCreateRecipe

--- client/src/Components/CreateRecipe.jsx
+++ client/src/Components/CreateRecipe.jsx
@@ -15,14 +15,26 @@ export default function CreateRecipe() {
   };
 
   const handleChangeDiets = (event) => {
+    // event.preventDefault();
+    event.target.checked
+      ? setInput((prev) => ({
+          ...prev,
+          diets: [...input.diets, event.target.value],
+        }))
+      : setInput((prev) => ({
+          ...prev,
+          diets: input.diets.filter((e) => e !== event.target.value),
+        }));
+  };
+
+  const handledSubmit = (event) => {
     event.preventDefault();
-    console.log(event);
   };
 
   return (
     <div>
       <h1>Crear Receta</h1>
-      <form>
+      <form onSubmit={handledSubmit}>
         <div>
           <label>Titulo</label>
           <input
@@ -67,32 +79,92 @@ export default function CreateRecipe() {
             onChange={(e) => handleChangeDiets(e)}
           />
           <label>Gluten Free</label>
-          <input type="checkbox" />
+
+          <input
+            type="checkbox"
+            name={"ketogenic"}
+            value={"ketogenic"}
+            onChange={(e) => handleChangeDiets(e)}
+          />
           <label>Ketogenic</label>
-          <input type="checkbox" />
+
+          <input
+            type="checkbox"
+            name={"vegetarian"}
+            value={"vegetarian"}
+            onChange={(e) => handleChangeDiets(e)}
+          />
           <label>Vegetarian</label>
-          <input type="checkbox" />
+
+          <input
+            type="checkbox"
+            name={"lacto-vegetarian"}
+            value={"lacto-vegetarian"}
+            onChange={(e) => handleChangeDiets(e)}
+          />
           <label>Lacto-Vegetarian</label>
-          <input type="checkbox" />
+
+          <input
+            type="checkbox"
+            name={"ovo-vegetarian"}
+            value={"ovo-vegetarian"}
+            onChange={(e) => handleChangeDiets(e)}
+          />
           <label>Ovo-Vegetarian</label>
-          <input type="checkbox" />
+
+          <input
+            type="checkbox"
+            name={"vegan"}
+            value={"vegan"}
+            onChange={(e) => handleChangeDiets(e)}
+          />
           <label>Vegan</label>
-          <input type="checkbox" />
+
+          <input
+            type="checkbox"
+            name={"pescetarian"}
+            value={"pescetarian"}
+            onChange={(e) => handleChangeDiets(e)}
+          />
           <label>Pescetarian</label>
-          <input type="checkbox" />
+
+          <input
+            type="checkbox"
+            name={"paleo"}
+            value={"paleo"}
+            onChange={(e) => handleChangeDiets(e)}
+          />
           <label>Paleo</label>
-          <input type="checkbox" />
+
+          <input
+            type="checkbox"
+            name={"primal"}
+            value={"primal"}
+            onChange={(e) => handleChangeDiets(e)}
+          />
           <label>Primal</label>
-          <input type="checkbox" />
+
+          <input
+            type="checkbox"
+            name={"low fodmap"}
+            value={"low fodmap"}
+            onChange={(e) => handleChangeDiets(e)}
+          />
           <label>Low FODMAP</label>
-          <input type="checkbox" />
+
+          <input
+            type="checkbox"
+            name={"whole30"}
+            value={"whole30"}
+            onChange={(e) => handleChangeDiets(e)}
+          />
           <label>Whole30</label>
         </div>
 
         <br />
 
         <div>
-          <input type="submit" name="Crear Receta" id="" />
+          <input type={"submit"} value={"Crear Receta"} id="" />
         </div>
       </form>
     </div>
